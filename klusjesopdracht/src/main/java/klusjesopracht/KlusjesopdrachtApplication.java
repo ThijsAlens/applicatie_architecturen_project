4d6bab68de7856a2f5cbd49b2004910fdc547663
Ubuntu - Thijs Alens <thijs.alens@telenet.be>
2024-12-01 15:03:37 +0100

    aanmaken van de opdracht

---
+++ klusjesopdracht/src/main/java/klusjesopracht/KlusjesopdrachtApplication.java
@@ -0,0 +1,13 @@
+package klusjesopracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlusjesopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlusjesopdrachtApplication.class, args);
+	}
+
+}
---
+++ klusjesopdracht/src/test/java/klusjesopracht/KlusjesopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package klusjesopracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlusjesopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
