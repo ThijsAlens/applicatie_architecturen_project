beb75f8da38065511db4dbefb5c696b1b8569055
Ubuntu - Thijs Alens <thijs.alens@telenet.be>
2024-12-04 11:36:25 +0100

    v2

---
+++ klusjes_v2/src/main/java/klusjes_v2/KlusjesV2Application.java
@@ -0,0 +1,13 @@
+package klusjes_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlusjesV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlusjesV2Application.class, args);
+	}
+
+}
