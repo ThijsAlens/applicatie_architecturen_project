18ed3fa3656de5352b759f4fc0e1dadd8f5decac
stijn <r0834588@kuleuven.be>
2024-12-21 16:12:14 +0100

    klant en people aangepast in model

--- klusjes_v2/src/main/java/klusjes_v2/model/Klant.java
+++ klusjes_v2/src/main/java/klusjes_v2/model/Klant.java
@@ -1,4 +1,40 @@
 package klusjes_v2.model;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
-public class Klant extends People{
+@Entity
+@Table(name = "klant")
+public class Klant {
+
+    @Id
+    @NotBlank
+    @Column(name = "KLANT_ID")
+    private Integer klantId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "people", referencedColumnName = "USERNAME")
+    private People people;
+
+    public Integer getKlantId() {
+        return klantId;
+    }
+
+    public void setKlantId(Integer klantId) {
+        this.klantId = klantId;
+    }
+
+    public People getPeople() {
+        return people;
+    }
+
+    public void setPeople(People people) {
+        this.people = people;
+    }
+    
 }
--- klusjes_v2/src/main/java/klusjes_v2/model/Klusjesman.java
+++ klusjes_v2/src/main/java/klusjes_v2/model/Klusjesman.java
@@ -1,20 +1,51 @@
 package klusjes_v2.model;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
-import java.util.ArrayList;
+@Entity
+@Table(name = "klusjesman")
+public class Klusjesman {
 
+    @Id
+    @NotBlank
+    @Column(name = "KLUSJESMAN_ID")
+    private Integer klantId;
 
-public class Klusjesman extends People{
-	private float rating;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "people", referencedColumnName = "USERNAME")
+    private People people;
+    
+    @Column(name="RATING")
+    private float rating;
 
-	public float getRating() {
+    public float getRating() {
 		return rating;
 	}
 
 	public void setRating(float rating) {
 		this.rating = rating;
 	}
-	
-	
+
+	public Integer getKlantId() {
+        return klantId;
+    }
+
+    public void setKlantId(Integer klantId) {
+        this.klantId = klantId;
+    }
+
+    public People getPeople() {
+        return people;
+    }
+
+    public void setPeople(People people) {
+        this.people = people;
+    }
+    
 }
-	
-	
