730623df528234e7ed038b5b518d5c63495573f1
stijn <r0834588@kuleuven.be>
2024-12-24 12:01:03 +0100

    fixed repo en services zodat het kan opstarten (werkt nog niet functioneel)

--- klusjes_v2/src/main/java/klusjes_v2/repositories/KlusjesRepository.java
+++ klusjes_v2/src/main/java/klusjes_v2/repositories/KlusjesRepository.java
@@ -1,10 +1,11 @@
 package klusjes_v2.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import klusjes_v2.model.Klusje;
+import klusjes_v2.model.Klus;
 import klusjes_v2.model.People;
 
-public interface KlusjesRepository extends JpaRepository<Klusje, Integer>{
+public interface KlusjesRepository extends JpaRepository<Klus, Integer>{
 
 }
